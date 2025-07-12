813ce29317f50457df21065475d444205700bf18
sagnik mondal <sagnikmondal011@gmail.com>
2025-07-12 23:27:24 +0530

    Add file

---
+++ pattern/Half_Pyramid.java
@@ -0,0 +1,12 @@
+ public class Half_Pyramid{
+    public static void main(String[] args) {
+        // print lines
+        for(int line=1;line<=4;line++){
+            // print numbers
+            for(int star=1;star<=line;star++){
+                System.out.print(star);
+            }
+        System.out.println();
+        }
+    }
+ }
