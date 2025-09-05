b65c0dfa49cd5ba3746f593968a8e456c0567e19
sagnik mondal <sagnikmondal011@gmail.com>
2025-09-05 21:41:22 +0530

    modify

--- pattern/Character.java
+++ pattern/Character.java
@@ -2,11 +2,12 @@
     public static void main(String[] args) {
         char ch='A';
         // print lines
-        for(int line=1;line<=4;line++){
+        for(int line=1;line<=5;line++){
             // print characters
             for(int star=1;star<=line;star++){
                 
-                System.out.print(ch);
+                System.out.print(ch +"\t");
+                // character update
                 ch++;
             }
         System.out.println();
