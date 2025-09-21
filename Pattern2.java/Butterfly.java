87da6619c9fbed42c71109a4c79091c6db7c2e58
Sagnik Mondal <sagnikmondal011@gmail.com>
2025-09-21 22:16:07 +0530

    Update Butterfly.java

--- Pattern2.java/Butterfly.java
+++ Pattern2.java/Butterfly.java
@@ -6,14 +6,17 @@ public class Butterfly{
         for(int i=1;i<=n;i++){
             
             // starts-i
+            
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             // spaces
+            
             for(int j=1;j<=2*(n-i);j++){
                 System.out.print(" ");
             }
             // stars-i
+            
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
