dc1e0f9d8ba2bfdd16210dbe0320d15e05a91af4
Sagnik Mondal <sagnikmondal011@gmail.com>
2025-09-23 23:29:47 +0530

    Update Income.java

--- Conditional_Statement/Income.java
+++ Conditional_Statement/Income.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Income{
     
     public static void main(String[] args) {
+        
         Scanner sc=new Scanner(System.in);
         int income= sc.nextInt();
         int tax;
