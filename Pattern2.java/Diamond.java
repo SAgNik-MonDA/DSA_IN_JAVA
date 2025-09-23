1118ec305a98d2e22a2df221516cc1fb97de0fba
Sagnik Mondal <sagnikmondal011@gmail.com>
2025-09-23 23:31:47 +0530

    Update Diamond.java

--- Pattern2.java/Diamond.java
+++ Pattern2.java/Diamond.java
@@ -5,22 +5,31 @@ public class Diamond{
         // 1st half
         
         for(int i=1;i<=n;i++){
+            
             // spaces
+            
             for(int j=1;j<=(n-i);j++)
             System.out.print(" ");
+            
             // star
+            
             for(int j=1;j<=(2*i)-1;j++){
                 System.out.print("*");
             }
             System.out.println(" ");
         }
+        
         // 2nd half
 
         for(int i=n;i>=1;i--){
+            
             // spaces
+            
             for(int j=1;j<=(n-i);j++)
             System.out.print("");
+            
             // star
+            
             for(int j=1;j<=(2*i)-1;j++){
                 System.out.print("*");
             }
