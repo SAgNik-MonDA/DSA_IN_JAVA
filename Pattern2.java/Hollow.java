11b0f27c315d04ef25bd7c941af2c21e7ba7c123
Sagnik Mondal <sagnikmondal011@gmail.com>
2025-09-29 23:05:22 +0530

    Update Hollow.java

--- Pattern2.java/Hollow.java
+++ Pattern2.java/Hollow.java
@@ -1,11 +1,15 @@
  class Hollow{
     public static void hollow(int totrows,int totcols){
+     
         // outer loops
         for(int i=1;i<=totrows;i++){
+         
             // inner-columns
             for(int j=1;j<=totcols;j++){
+             
                 // cell-(i,j)
                 if(i==1||i==totrows||j==1||j==totcols){
+                 
                     // boundary cells
                     System.out.print("*");
                 }
