f5fb54e264c783db4d5aeadfa6f83469085887eb
sagnik mondal <sagnikmondal011@gmail.com>
2026-03-14 03:38:35 +0530

    modify in pattern2

--- Pattern2.java/Diamond.java
+++ Pattern2.java/Diamond.java
@@ -10,13 +10,13 @@ public class Diamond{
                 System.out.print("*");
             }
             System.out.println(" ");
-        }
+        } 
         // 2nd half
 
         for(int i=n;i>=1;i--){
             // spaces
             for(int j=1;j<=(n-i);j++)
-            System.out.print("");
+            System.out.print(" ");
             // star
             for(int j=1;j<=(2*i)-1;j++){
                 System.out.print("*");
