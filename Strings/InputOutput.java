31ed10a36b7d832ba7a034b27d22594f12004b8b
sagnik mondal <sagnikmondal011@gmail.com>
2026-04-01 01:51:47 +0530

    add 2D array

---
+++ Strings/InputOutput.java
@@ -0,0 +1,48 @@
+
+
+public class InputOutput{
+
+    public static void printLetters(String str){
+        for(int i=0;i<str.length();i++){
+            System.err.print(str.charAt(i)+ " ");
+        }
+        System.err.println("");
+    }
+
+
+    public static void main(String[] args) {
+        //  String str = "abcd";
+        // String str2 = new String("xyz");
+
+
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+        // name=sc.nextLine();
+        // System.err.println(name);
+
+
+        // Find out length
+
+        // String fullName = "Tony Stark";
+        // System.err.println(fullName.length());
+
+
+        // Concatenation
+
+        String FristName = "Sagnik";
+        String LastName = "Mondal";
+        String FullName = FristName + " "+ LastName;
+
+        System.err.println(FullName);
+
+
+        // Find String Character
+        System.err.println(FullName.charAt(0));
+
+        printLetters(FullName);
+
+
+        
+
+    }
+}
