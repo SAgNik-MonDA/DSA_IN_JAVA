f3d073b6ed851cf1fa922b96b90aa599205d3fa3
sagnik mondal <sagnikmondal011@gmail.com>
2026-04-15 20:21:14 +0530

    add Strings

---
+++ Strings/Assignment/Question1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Question1{
+    public static void main(String[] args) {
+        System.out.print("Enetr a String : ");
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int count = 0;
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                count++;
+            }
+        }
+        System.out.println("Count Of Vowel is :"+count);
+    }
+}
